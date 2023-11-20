3727c985055813d48bf1a4d4757f399c8e5ecbf5
lucas <lucas@phala.network>
2023-11-20 21:50:46 +0800

    ui for layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <main>喝咯我忍了都</main>;
+  return (
+    <main className="mx-auto flex max-w-7xl gap-3">
+      <div className="flex-1">
+        <textarea className="min-h-screen w-full border-2 p-2" />
+      </div>
+      <div className="flex-1">
+        <textarea className="min-h-screen w-full border-2 p-2" />
+      </div>
+    </main>
+  );
 }
