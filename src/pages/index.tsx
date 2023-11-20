c90cab79d6034ab74dd2feb0aa63f031e744fbb8
lucas <lucas@phala.network>
2023-11-20 21:58:23 +0800

    source

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,28 @@
+import { useState } from "react";
+
 export default function Home() {
+  const [source, setSource] =
+    useState(`color: var(--light-neutral-color-text-text-1, #262933);
+
+  /* Web/SL/600/H5 24px */
+  font-family: Gate Sans 2.0;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;`);
+
+  console.log(source);
+
   return (
     <main className="mx-auto flex max-w-7xl gap-3">
       <div className="flex-1">
-        <textarea className="min-h-screen w-full border-2 p-2" />
+        <textarea
+          className="min-h-screen w-full border-2 p-2"
+          value={source}
+          onChange={(e) => {
+            setSource(e.target.value);
+          }}
+        />
       </div>
       <div className="flex-1">
         <textarea className="min-h-screen w-full border-2 p-2" />
