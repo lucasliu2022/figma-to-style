58d38eb9b2c09365b9ba607a24d78ab32efbdf17
lucas <lucas@phala.network>
2023-12-14 15:40:35 +0800

    tree view

--- src/pages/design/index.tsx
+++ src/pages/design/index.tsx
@@ -1,6 +1,47 @@
 import PropertyPanel from "./PropertyPanel";
 import Panel from "./panel";
 import TreePanel from "./treePanel";
+import { atomWithReducer } from "jotai/utils";
+
+const uuid = () => {
+  return Date.now().toString(32) + Math.random().toString(16);
+};
+
+function traverse(comp: any, id: any, type: String) {
+  if (comp.id !== id) {
+    return {
+      ...comp,
+      children: comp?.children?.map((item: any) => traverse(item, id, type)),
+    };
+  }
+  if (type === "createPage") {
+    const children = comp?.children || [];
+    return {
+      ...comp,
+      children: [{ id: uuid(), name: "Untitle" }, ...children],
+    };
+  } else {
+    return comp;
+  }
+}
+
+const compReducer = (prev: any, action: any) => {
+  console.log(action);
+  if (action.type === "createPage") {
+    const id = action.payload;
+    return traverse(prev, id, "createPage");
+  }
+  return prev;
+};
+
+export const compAtom = atomWithReducer(
+  {
+    id: uuid(),
+    name: "root",
+    children: [],
+  },
+  compReducer,
+);
 
 export default function Design() {
   return (
--- src/pages/design/treePanel.tsx
+++ src/pages/design/treePanel.tsx
@@ -1,3 +1,59 @@
+import { useAtom } from "jotai";
+import { compAtom } from ".";
+import { useState } from "react";
+
 export default function TreePanel() {
-  return <div className="basis-1/5">treepanel</div>;
+  const [page] = useAtom(compAtom);
+  return (
+    <div className="h-full basis-1/5 overflow-auto">
+      <TreeItem {...page} />
+    </div>
+  );
+}
+
+function TreeItem(comp: any) {
+  const [page, dispatch] = useAtom(compAtom);
+  const [coll, setColl] = useState(false);
+  return (
+    <div className="">
+      <div
+        className="flex select-none items-center bg-blue-700 p-2 text-white hover:bg-blue-800"
+        onClick={() => setColl(!coll)}
+      >
+        <div className="flex-1">{comp.name}</div>
+        <div
+          className="cursor-pointer"
+          onClick={(e) => {
+            e.stopPropagation();
+            dispatch({
+              type: "createPage",
+              payload: comp.id,
+            });
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-5 w-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+            />
+          </svg>
+        </div>
+      </div>
+      {coll ? null : (
+        <div className="pl-2">
+          {comp?.children?.map((item: any) => {
+            return <TreeItem {...item} key={item.id} />;
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
